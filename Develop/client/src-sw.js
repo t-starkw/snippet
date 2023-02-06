ea129823aaa3be1f551e6c631ce3f7993d8ddd54
Tessa <tess.starkweather@gmail.com>
2023-02-05 21:17:25 -0500

    filled in missing code

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -1,4 +1,5 @@
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
+const { StaleWhileRevalidate } = require('workbox-strategies');
 const { CacheFirst } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
@@ -27,4 +28,13 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
+    cacheName: 'asset-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
